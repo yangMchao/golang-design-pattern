57514b79ac99e63860405fead29d658d28fdf6b3
yangchao <yangchao@zetyun.com>
2024-07-18 22:11:36 +0800

    feat(prototype): prototype

--- 07_prototype/prototype_test.go
+++ 07_prototype/prototype_test.go
@@ -1,11 +1,31 @@
 package prototype
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
+
+func init() {
+	manager = NewPrototypeManager()
+
+	t1 := &Type1{
+		name: "type1",
+		D: &DCopy{
+			dDCopy: "d",
+		},
+	}
+	manager.Set("t1", t1)
+}
 
 var manager *PrototypeManager
 
+type DCopy struct {
+	dDCopy string
+}
+
 type Type1 struct {
 	name string
+	D    *DCopy
 }
 
 func (t *Type1) Clone() Cloneable {
@@ -20,6 +40,20 @@ type Type2 struct {
 func (t *Type2) Clone() Cloneable {
 	tc := *t
 	return &tc
+}
+
+func TestCopy(t *testing.T) {
+	dCopy := &DCopy{
+		dDCopy: "d",
+	}
+	t1 := &Type1{
+		name: "type1",
+		D:    dCopy,
+	}
+	tc := *t1
+	t1.D.dDCopy = "c"
+	log.Println(tc.D.dDCopy)
+	log.Println(t1.D.dDCopy)
 }
 
 func TestClone(t *testing.T) {
@@ -40,13 +74,4 @@ func TestCloneFromManager(t *testing.T) {
 		t.Fatal("error")
 	}
 
-}
-
-func init() {
-	manager = NewPrototypeManager()
-
-	t1 := &Type1{
-		name: "type1",
-	}
-	manager.Set("t1", t1)
 }
