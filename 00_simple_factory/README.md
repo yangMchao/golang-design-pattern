9b812990cdd5bb793af803bb11ac3da9ec55e32c
yangchao <yangchao@zetyun.com>
2024-07-18 17:27:14 +0800

    feat(simple factory): simple factory learning

--- 00_simple_factory/README.md
+++ 00_simple_factory/README.md
@@ -4,3 +4,5 @@
 `NewXXX` 函数返回接口时就是简单工厂模式，也就是说 `Golang` 的一般推荐做法就是简单工厂。
 
 在这个 `simplefactory` 包中只有API 接口和 `NewAPI` 函数为包外可见，封装了实现细节。
+
+sync.once 实现安全单例
--- 00_simple_factory/simple_test.go
+++ 00_simple_factory/simple_test.go
@@ -1,20 +1,29 @@
 package simplefactory
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"testing"
+)
 
-// TestType1 test get hiapi with factory
+// TestType1 test get hi api with factory
 func TestType1(t *testing.T) {
 	api := NewAPI(1)
 	s := api.Say("Tom")
+	typeOf := reflect.TypeOf(api)
 	if s != "Hi, Tom" {
 		t.Fatal("Type1 test fail")
 	}
+	log.Println(fmt.Sprintf("type %s , value %s", typeOf, s))
 }
 
 func TestType2(t *testing.T) {
 	api := NewAPI(2)
 	s := api.Say("Tom")
-	if s != "Hello, Tom" {
-		t.Fatal("Type2 test fail")
+	typeOf := reflect.TypeOf(api)
+	if s != "Hi, Tom" {
+		t.Fatal("Type1 test fail")
 	}
+	log.Println(fmt.Sprintf("type %s , value %s", typeOf, s))
 }
