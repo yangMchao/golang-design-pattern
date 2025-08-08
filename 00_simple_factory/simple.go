62a03b6a69a8a94622359db107b1480f4b35812e
yangchao <yangchao@zetyun.com>
2025-08-08 15:45:11 +0000

    refactor(simple): 使用switch替代if-else条件判断

--- 00_simple_factory/simple.go
+++ 00_simple_factory/simple.go
@@ -9,9 +9,10 @@ type API interface {
 
 // NewAPI return Api instance by type
 func NewAPI(t int) API {
-	if t == 1 {
+	switch t {
+	case 1:
 		return &hiAPI{}
-	} else if t == 2 {
+	case 2:
 		return &helloAPI{}
 	}
 	return nil
